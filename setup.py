9fca970c022d9d687b5cf91aa727812ab20d5089
Sabra Bildoeau <sabrabilodeau@gmail.com>
2021-01-12 18:13:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name="pyutils",
       version="0.1",
       description="Test for pip install git+",
-      url="https://github.com/kylemcdonald/python-utils",
+      url="https://github.com/Cryliss/python-utils.git",
       install_requires=["numpy", "opencv-python", "Pillow"],
       packages=find_packages()
 )
